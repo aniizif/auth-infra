96bacd049d8bf9a3b08abdceeb2144b87b4d509a
aniizif <szhecka127@mail.ru>
2025-08-05 02:13:17 +0500

    UPDATE: remove todo from file

--- server/auth-service/pkg/jwt/jwt.go
+++ server/auth-service/pkg/jwt/jwt.go
@@ -17,10 +17,3 @@ func GenerateToken(userID int) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(Secret)
 }
-
-// TODO: Relocate to forum-service
-//func ValidateToken(tokenString string) (*jwt.Token, error) {
-//	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-//		return Secret, nil
-//	})
-//}
